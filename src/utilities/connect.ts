f56098dd9468f2a8802152ecb0b0f6796f10cc0b
ahmedmlaouhia <ahmedmlaouhia6@gmail.com>
2022-02-18 12:57:38 +0100

    well

--- src/utilities/connect.ts
+++ src/utilities/connect.ts
@@ -2,7 +2,7 @@ import { connect as cnt } from "mongoose";
 
 const connect = async (callback: () => void) => {
   try {
-    await cnt(`${process.env.MONGODB_URI}`);
+    await cnt("mongodb://172.19.0.2:27017/SampleDB");
     console.log("Connected to MongoDB");
     callback();
   } catch (error) {
